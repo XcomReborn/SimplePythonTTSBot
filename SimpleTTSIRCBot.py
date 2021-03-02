a98230e1f8195d0049808ce4107186b87c5d7e18
XcomReborn <zachary.coldrick@gmail.com>
2021-03-02 23:11:39 +0000

    consolidated TTS class into single file

--- SimpleTTSIRCBot.py
+++ SimpleTTSIRCBot.py
@@ -4,7 +4,7 @@ import logging # for logging errors to file
 import sys # for file input and output
 import threading # for the creation of separate threads to do several things at once while one thread is buzy
 import queue # for inter thread communication
-import TTS # wrapper for the pyttsx3 package
+#import TTS # wrapper for the pyttsx3 package
 import json # for saving and loading to json files
 import os # for operating system functions
 import re # for regular expressions
@@ -582,7 +582,7 @@ class ttsSpeech(threading.Thread):
 		self.ttsReady = ttsReady
 		threading.Thread.__init__(self)
 		self.queue = queue.Queue()
-		self.tts = TTS.TTSThread(ttsReady = self.ttsReady)
+		self.tts = TTSThread(ttsReady = self.ttsReady)
 		self.previousUserName = ""
 		self.running = True
 
@@ -625,7 +625,93 @@ class ttsSpeech(threading.Thread):
 		self.running = False
 		if self.tts:
 			self.tts.terminate()
-	
+
+class TTSThread(threading.Thread):
+    def __init__(self, ttsReady = None, rate=115, event=None):
+        super().__init__()
+
+        if event:
+            setattr(self, event, threading.Event())
+
+        self.ttsReady = ttsReady
+        
+        self._cancel = threading.Event()
+        self.rate = rate
+        self.engine = None
+
+        self._say = threading.Event()
+        self._text_lock = threading.Lock()
+        self._text = []
+
+        self._is_alive = threading.Event()
+        self._is_alive.set()
+        self.start()
+
+    def _init_engine(self, rate):
+        engine = pyttsx3.init()
+        engine.setProperty('rate', rate)  # setting up new voice rate    
+        engine.connect('finished-utterance', self._on_completed)
+        engine.connect('started-word', self._on_cancel)
+        return engine
+
+    def say(self, text, stop=None):
+        if self._is_alive.is_set():
+            self._cancel.clear()
+
+            if isinstance(text, str):
+                text = [(text, stop)]
+
+            if isinstance(text, (list, tuple)):
+                for t in text:
+                    if isinstance(t, str):
+                        t = t, None
+
+                    with self._text_lock:
+                        self._text.append(t)
+
+                    self._say.set()
+
+    def cancel(self):
+        self._cancel.set()
+
+    def _on_cancel(self, name, location, length):
+        if self._cancel.is_set():
+            self.stop()
+
+    def stop(self):        
+        self.engine.stop()
+        time.sleep(0.5)
+        self.engine.endLoop()
+
+    def _on_completed(self, name, completed):
+        if completed:
+            self.engine.endLoop()
+            self.on_finished_utterance(name, completed)
+
+    def on_finished_utterance(self, name, completed):
+        print("on_finished_utterance was called") 
+        print(self.ttsReady)
+        if self.ttsReady:
+            self.ttsReady.set()
+            print ("ttsReady was set to {}".format(str(self.ttsReady.is_set())))
+        self.engine.endLoop()
+
+    def terminate(self):
+        self._is_alive.clear()
+        self._cancel.set()
+        self.join()
+
+    def run(self):
+        self.engine = engine = self._init_engine(self.rate)
+        while self._is_alive.is_set():
+            while self._say.wait(0.1):
+                self._say.clear()
+
+                while not self._cancel.is_set() and len(self._text):
+                    with self._text_lock:
+                        engine.say(*self._text.pop(0))
+                    engine.startLoop()
+
 
 # The program propper starts here
 
